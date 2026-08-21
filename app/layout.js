fe55e8ff74c553e45dca812c0d4314d91ce8899c
hanjinghong626-ctrl <hanjinghong626@gmail.com>
2026-08-21 18:35:23 +0300

    refactor: clean layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Cormorant_Garamond } from 'next/font/google'
 
 const cormorant = Cormorant_Garamond({
   subsets: ['latin', 'cyrillic'],
-  weight: ['400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '700'],
   style: ['normal', 'italic'],
   display: 'swap',
   variable: '--font-cormorant',
@@ -11,7 +11,7 @@ const cormorant = Cormorant_Garamond({
 
 export const metadata = {
   title: '俄罗斯音乐百科 | Энциклопедия русской музыки',
-  description: '从格林卡到肖斯塔科维奇，1665条中俄双语音乐术语百科。涵盖音乐理论、作曲家、乐器、歌剧、芭蕾等20个分类，4333条交叉引用。',
+  description: '从格林卡到肖斯塔科维奇，1665条中俄双语音乐术语百科。',
 }
 
 export default function RootLayout({ children }) {
